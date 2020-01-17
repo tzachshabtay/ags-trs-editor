2f9814c670d4291b6a5e0031cbb19cfb0c7e7482
Tzach Shabtay <tzachshabtay@gmail.com>
2020-01-17 00:04:32 -0500

    Updates

---
+++ precache-manifest.6a69d65b3f0205881f370aaf1d377046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148f4e152029978956ce79010b4b112c",
+    "url": "/ags-trs-editor/index.html"
+  },
+  {
+    "revision": "2b00afe70a57d2676012",
+    "url": "/ags-trs-editor/static/css/main.7f527e1e.chunk.css"
+  },
+  {
+    "revision": "6a1a3406a0ab1b8c1a47",
+    "url": "/ags-trs-editor/static/js/2.cf86c82d.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/ags-trs-editor/static/js/2.cf86c82d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b00afe70a57d2676012",
+    "url": "/ags-trs-editor/static/js/main.059556ae.chunk.js"
+  },
+  {
+    "revision": "dce435d8f9922a70b2a3",
+    "url": "/ags-trs-editor/static/js/runtime-main.ca227238.js"
+  }
+]);
